378756d3ccdda5f97225ec37b0901309320591a9
kidsamort <kidsamort@gmail.com>
2022-08-25 21:07:48 +0300

    heroku delete

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,6 +1,7 @@
 import { EmptyProps } from 'components/empty/empty.props'
 import styles from 'components/contact/contact.module.css'
 import CardsContact from 'components/UI/cardsContact'
+import Title from 'components/UI/title'
 
 
 const Contact = ({ tel, mail, loc }: EmptyProps): JSX.Element => {
@@ -8,6 +9,7 @@ const Contact = ({ tel, mail, loc }: EmptyProps): JSX.Element => {
 
   return (
     <div className={styles.wrapper}>
+      <Title size={'md'}>Контакты</Title>
       <CardsContact title={'Телефоны'} contact={tel} />
       <CardsContact title={'Email Адрес'}
                     contact={mail} />
