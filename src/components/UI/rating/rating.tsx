5b40b6ae8f5bdf50cf68f60186de0b0b170db7a8
kidsamort <kidsamort@gmail.com>
2022-08-30 01:39:57 +0300

    fix bug

--- src/components/UI/rating/rating.tsx
+++ src/components/UI/rating/rating.tsx
@@ -2,23 +2,23 @@ import React from 'react'
 import styles from './empty.module.css'
 import { RatingProps } from 'components/UI/rating/rating.props'
 import { useEffect, useState } from 'react'
-import { ReactComponent as StarIcon } from 'components/UI/rating/star.svg'
+// import { ReactComponent as StarIcon } from 'components/UI/rating/star.svg'
 
 const Rating = ({ rating }: RatingProps): JSX.Element => {
   const [ratingArr, setRatingArr] = useState<JSX.Element[]>(new Array(0).fill(<></>))
 
-  useEffect(() => {
-    const ratingCreate = (rate: number) => {
-      const updateArray = ratingArr.map((item, index) => {
-        return (
-          <StarIcon key={index} style={{ fill: index < rate ? 'red' : 'gray' }}
-          />
-        )
-      })
-      setRatingArr(updateArray)
-    }
-    ratingCreate(rating)
-  }, [rating, ratingArr])
+  // useEffect(() => {
+  //   const ratingCreate = (rate: number) => {
+  //     const updateArray = ratingArr.map((item, index) => {
+  //       return (
+  //         <StarIcon key={index} style={{ fill: index < rate ? 'red' : 'gray' }}
+  //         />
+  //       )
+  //     })
+  //     setRatingArr(updateArray)
+  //   }
+  //   ratingCreate(rating)
+  // }, [rating, ratingArr])
 
 
   return (
