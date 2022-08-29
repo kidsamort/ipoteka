5f807a25c4a3187a26eb7145846c04ddbb92e0ea
kidsamort <kidsamort@gmail.com>
2022-08-29 17:58:16 +0300

    maps fixed

--- src/components/map/maps.tsx
+++ src/components/map/maps.tsx
@@ -1,12 +1,19 @@
 import styles from 'components/map/maps.module.css'
 import { MapsProps } from 'components/map/maps.props'
 import { Map, Placemark, YMaps, ZoomControl } from 'react-yandex-maps'
+import { useEffect, useState } from 'react'
 
 const Maps = ({ loc }: MapsProps): JSX.Element => {
+  const [width, setWidth] = useState<number>(0)
+  const [height, setHeight] = useState<number>(0)
+
+  useEffect(() => {
+    setHeight(window.innerHeight)
+  }, [])
   return (
     <div className={styles.wrapper}>
       <YMaps style={{ width: '100%' }}>
-        <Map width={'100%'} height={'500px'}
+        <Map width={'100%'} height={`${height / 2}px`}
              instanceRef={ref => { // @ts-ignore
                ref && ref.behaviors.disable('scrollZoom')
              }}
